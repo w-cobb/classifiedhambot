d88bfd3787b2346de57f39dfa09765683b3238aa
Alex Cobb <williamcobb930@gmail.com>
2025-07-23 15:53:57 -0700

    Create the docker compose file and make sure the images build

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,14 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-URL = "https://swap.qth.com/all.php"
-
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0"
 }
 
-page = requests.get(URL, headers=headers)
+url = "https://swap.qth.com/all.php"
+
+page = requests.get(url, headers=headers)
 print(page.status_code)
 
 soup = BeautifulSoup(page.content, "html.parser")
