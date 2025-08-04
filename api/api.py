e64ef472a0e7fdff88878a8fb0265ae16187949d
Alex Cobb <williamcobb930@gmail.com>
2025-08-04 16:35:22 -0700

    Added basic endpoints

--- api/api.py
+++ api/api.py
@@ -3,5 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def root():
+async def root():
     return {'message': 'Hello World'}
+
+@app.get('/trackers')
+async def get_trackers():
+    return {'trackers': ['test1', 'test2', 'test3']}
+
+@app.get('/listings')
+async def get_listings():
+    return {'listings': ['listing1','listing2','listing3']}
+
+@app.get('/alerts')
+async def get_alerts():
+    return {'alerts': ['alert1','alert2','alert3']}
